b813c4103a984791fc5d8b5c9fac3c09f2fb88f4
Robert Kadak <rokada@taltech.ee>
2025-10-26 16:52:37 +0200

    UI demo

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,29 @@
 #include <iostream>
 #include <string>
+#include "ui/Menu.h"
+#include "ui/MenuItem.h"
 
+void lol() {}
 
 int main() {
+    Menu menu({
+        MenuItem("File", {
+            MenuItem("New", lol),
+            MenuItem("Open", lol),
+            MenuItem("Save", lol),
+            MenuItem("Exit", lol),
+        }),
+        MenuItem("Edit", lol),
+        MenuItem("Selection", lol),
+        MenuItem("View", lol),
+    });
+
+    menu.displayHorizontal();
+    menu.selectPrev();
+    menu.displayHorizontal();
+    menu.selectNext();
+    menu.select();
+    menu.displayHorizontal();
+
     return 0;
 }
