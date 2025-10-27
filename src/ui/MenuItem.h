f49e6a757433be5815d16d7c92a5aeed4f6636b4
Robert Kadak <rokada@taltech.ee>
2025-10-27 18:56:17 +0200

    fix: use std::function

--- src/ui/MenuItem.h
+++ src/ui/MenuItem.h
@@ -3,16 +3,17 @@
 
 #include <string>
 #include <vector>
+#include <functional>
 
 class MenuItem {
 public:
     std::string label;
     std::vector<MenuItem> items;
-    void (*action)() = nullptr;
+    std::function<void()> action = nullptr;
 
-    MenuItem(std::string label, void (*action)());
+    MenuItem(std::string label, std::function<void()> action);
     MenuItem(std::string label, std::vector<MenuItem> items);
-    MenuItem(std::string label, std::vector<MenuItem> items, void (*action)());
+    MenuItem(std::string label, std::vector<MenuItem> items, std::function<void()> action);
 
     std::string toString() const;
 };
