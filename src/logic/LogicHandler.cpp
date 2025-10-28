26c3b37caa07f047f485dc347277de06409003ba
Daniel Zapryanov <dazapr@taltech.ee>
2025-10-28 16:52:15 +0200

    fix: logichandler bs

--- src/logic/LogicHandler.cpp
+++ src/logic/LogicHandler.cpp
@@ -2,23 +2,13 @@
 #include <memory>
 #include <vector>
 
-std::unique_ptr<User> log_in(std::string u_id, std::string passwd){
-    //TODO
-    return std::make_unique<User>();
-}
-
-std::unique_ptr<User> register_user(std::string u_id, std::string psswd){
-    //TODO
-    return std::make_unique<User>();
-}
-
-std::vector<std::shared_ptr<Listing>> get_filtered(std::vector<std::shared_ptr<Listing>> to_filter, Category category){
+std::vector<std::shared_ptr<Listing>> LogicHandler::get_filtered(std::vector<std::shared_ptr<Listing>> to_filter, Category category){
     //TODO
     std::vector<std::shared_ptr<Listing>> v;
     return v;
 }
 
-std::vector<std::shared_ptr<Listing>> get_sorted(std::vector<std::shared_ptr<Listing>> to_sort, std::string field){
+std::vector<std::shared_ptr<Listing>> LogicHandler::get_sorted(std::vector<std::shared_ptr<Listing>> to_sort, std::string field){
     //TODO
     std::vector<std::shared_ptr<Listing>> v;
     return v;
