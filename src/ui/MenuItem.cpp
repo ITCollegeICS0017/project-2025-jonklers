f8c7644df518b2760e0715900d21135502f1d815
Robert Kadak <rokada@taltech.ee>
2025-10-29 00:39:31 +0200

    refactor: MenuItem based on pointers

--- src/ui/MenuItem.cpp
+++ src/ui/MenuItem.cpp
@@ -1,10 +1,7 @@
 #include "MenuItem.h"
 
-MenuItem::MenuItem(std::string label, std::vector<MenuItem> items, std::function<void()> action) : label(label), items(items), action(action) {}
+MenuItem::MenuItem(std::string label, std::vector<std::shared_ptr<MenuItem>> items, std::function<void()> action) : label(label), items(items), action(action) {}
 
 std::string MenuItem::toString() const {
-    std::string result;
-    result += label;
-    if (!items.empty()) result += ""; // TODO: change folder display notation
-    return result;
-}
+    return label;
+};
